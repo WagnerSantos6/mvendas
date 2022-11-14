c001c670eda7bb8401b1ca0e1afc7113797c2086
Wagner Santos <wagnerstss@gmail.com>
2022-11-14 13:38:37 -0300

    Created first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import NotificationButton from "./components/NotificationButton/Index"
 
 
 function App() {
-return(
-  <h1>Olá Koopa, tudo bem?</h1>
-)
+  return (
+    <>
+      <h1>Olá Koopa, tudo bem?</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/components/NotificationButton/Index.tsx
@@ -0,0 +1,15 @@
+import icon from "../../assets/img/notification-icon.svg"
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <>
+            <div className="vendasmeta-red-btn">
+                <img src={icon} alt="Notificar" />
+            </div>
+        </>
+    )
+}
+
+export default NotificationButton
